16dff71e177c6a9a96d5703d9b659137dbc1d210
Bob Yuwono <bobywn@gmail.com>
2022-12-12 15:19:13 +0700

    delete 2 bucket

--- backend/main.tf
+++ backend/main.tf
@@ -7,14 +7,4 @@ terraform {
   }
 }
 
-resource "google_storage_bucket" "one" {
-  name = "bucket-boyo-1"
-  storage_class = "REGIONAL"
-  location = "us-east1"
-}
 
-resource "google_storage_bucket" "three" {
-  name = "bucket-boyo-3"
-  storage_class = "REGIONAL"
-  location = "us-east1"
-}
