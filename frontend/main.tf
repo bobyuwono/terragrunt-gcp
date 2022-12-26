00de4d1a916fe40a82159a9d79204a47955a5684
Bob Yuwono <bobywn@gmail.com>
2022-12-26 21:57:59 +0700

    Update main.tf

--- frontend/main.tf
+++ frontend/main.tf
@@ -24,6 +24,11 @@ resource "google_storage_bucket" "three" {
   location = "us-east1"
 }
 
+resource "google_storage_bucket" "twenty-two" {
+  name = "bucket-bobywn-22"
+  storage_class = "REGIONAL"
+  location = "us-east1"
+}
 
 
 # resource "google_compute_network" "vpc_network" {
