fcbc6ff7205885fb756e9ef937b736b1d186bef8
Bob Yuwono <bobywn@gmail.com>
2022-12-29 00:45:55 +0700

    Create dev-atlantis.sh

---
+++ dev-atlantis.sh
@@ -0,0 +1,10 @@
+echo "
+version: 3
+projects:
+- name: $HEAD_BRANCH_NAME
+  dir: $HEAD_BRANCH_NAME
+  workspace: $HEAD_BRANCH_NAME
+  workflow: terragrunt
+  autoplan:
+    enabled: true   
+" > atlantis.yaml
